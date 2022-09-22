b896e966754ecc26d0a27b2c4732b406466149e3
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2022-09-22 19:25:13 -0300

    Se renombró el MainActivity.kt y main_activity.xml, a UserLogin, ambos respectivamente.

--- app/src/main/java/com/example/v_foodapplication/UserLogin.kt
+++ app/src/main/java/com/example/v_foodapplication/UserLogin.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class UserLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_login)
 
         //val til_email = findViewById<TextInputLayout>(R.id.til_email)
         //val til_password = findViewById<TextInputLayout>(R.id.til_password)
@@ -16,11 +16,11 @@ class MainActivity : AppCompatActivity() {
         val btn_login = findViewById<Button>(R.id.btn_login)
 
         btn_register.setOnClickListener {
-            val intent = Intent(this@MainActivity,UserRegister::class.java)
+            val intent = Intent(this@UserLogin,UserRegister::class.java)
             startActivity(intent)
         }
         btn_login.setOnClickListener {
-            val intent = Intent(this@MainActivity,Category::class.java)
+            val intent = Intent(this@UserLogin,Category::class.java)
             startActivity(intent)
         }
     }
