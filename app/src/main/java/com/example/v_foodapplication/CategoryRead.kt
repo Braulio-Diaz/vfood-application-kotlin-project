be1c9d85f4866dcb9669edb88c6a8a90036b4250
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2022-09-22 20:03:11 -0300

    Se creó el kt y xml de ProductDelete con su funcionalidad de moverse entre vistas.

--- app/src/main/java/com/example/v_foodapplication/CategoryRead.kt
+++ app/src/main/java/com/example/v_foodapplication/CategoryRead.kt
@@ -1,11 +1,20 @@
 package com.example.v_foodapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CategoryRead : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_read)
+
+        val fab_product_delete = findViewById<FloatingActionButton>(R.id.fab_product_delete)
+
+        fab_product_delete.setOnClickListener {
+            val intent = Intent(this@CategoryRead,ProductDelete::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/v_foodapplication/UserRegister.kt
+++ app/src/main/java/com/example/v_foodapplication/UserRegister.kt
@@ -1,11 +1,20 @@
 package com.example.v_foodapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class UserRegister : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
+
+        val btn_form_register = findViewById<Button>(R.id.btn_form_register)
+
+        btn_form_register.setOnClickListener {
+            val intent = Intent(this@UserRegister,UserLogin::class.java)
+            startActivity(intent)
+        }
     }
 }
